fac1b9d42f684a343574bb4504bdb6a63e05f343
Gayatri Chippawar <gayatrichippawar@gmail.com>
2026-03-12 08:19:14 +0530

    Create Mirror.java

---
+++ Mirror.java
@@ -0,0 +1,46 @@
+package Binary_Tree;
+
+public class Mirror {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        public boolean isSymmetric(Node root) {
+            if(root==null){
+                return true;
+            }
+            return isMirror(root.left , root.right);
+        }
+
+        public boolean isMirror(Node left , Node right) {
+            if (left == null && right == null) {
+                return true;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.data != right.data) {
+                return false;
+            }
+            return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(2);
+        root.left.left = new Node(3);
+        root.left.right= new Node(4);
+        root.right.left = new Node(4);
+        root.right.right= new Node(3);
+
+        System.out.println(root.isSymmetric(root));
+    }
+}
